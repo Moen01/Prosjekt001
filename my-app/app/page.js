b8b1c729c78cd664c4106dcd27619e32f4c33973
Moen01 <143611053+Moen01@users.noreply.github.com>
2025-01-20 15:36:42 +0100

    nye funksjoner til knapp 1

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,5 +1,6 @@
 
 import styles from './page.module.css';
+import Header from './components/Header';
 
 export default function func() {
   return (
@@ -11,14 +12,7 @@ export default function func() {
       <div className={styles.separator}></div>
 
       <div className={styles.rightPane}>
-        <header className={styles.header}>
-          {Array.from({ length: 7 }, (_, i) => (
-            <div key={i} className={`${styles.button} ${styles[`button${i + 1}`]}`}>
-              <button className={styles[`button${i + 1}`]}>Button {i + 1}</button>
-              <p>Button {i + 1}</p>
-            </div>
-          ))}
-        </header>
+        <Header />
         <h2>This is the right pane</h2>
         
       </div>
