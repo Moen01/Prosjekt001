473a7dee44e2a0c8c0dcfd5bb6306aa6dd318f03
Moen01 <143611053+Moen01@users.noreply.github.com>
2025-03-08 22:04:22 +0100

    v2 box impoementert med nye strukturer

--- my-app/app/ProjectManagment/page.jsx
+++ my-app/app/ProjectManagment/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Box from '../components/Layout/box';
+import Box from '../components/box/page';
 import Square from '../components/Layout/Square';
 import './ProjectManagment.css';
 
 export default function ProsjektManagment() {
   return (
-    
+
       <Box>
         <Square />
         <Box> <Square> </Square></Box>
---
+++ my-app/app/components/box/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./box.css";
+
+export default function Box() {
+  return (
+    <div className="container">
+      <div className="box">This is a centered boxx
+      </div>
+    </div>
+  );
+}
