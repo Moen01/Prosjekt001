ffb400f038b0b24774bed0d6e6497c3711c26f75
Moen01 <143611053+Moen01@users.noreply.github.com>
2025-04-06 18:25:30 +0200

    v2 commit

--- my-app/app/main/pageHoyre.jsx
+++ my-app/app/main/pageHoyre.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import styles from '../styles/page.module.css';
 import Header from '../components/Layout/Header';
 import Delay from '../components/Layout/delay';
-import Ffmea from '../(pre-prod)/ffmea/page';
+//import Ffmea from '../(pre-prod)/ffmea/page';
 
 export default function PageRight() {
   
     return (
         <div className={styles.rightPane}>
         <Header />
-        <Ffmea />
-        <Square />
+       
+        
       </div> 
     );
 }
